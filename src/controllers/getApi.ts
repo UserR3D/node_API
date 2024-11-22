d1275c359e23f9a960a4866444cd16945ba85cca
Red <kauawar04@gmail.com>
2024-11-22 09:44:15 -0300

    number req.params

--- src/controllers/getApi.ts
+++ src/controllers/getApi.ts
@@ -3,7 +3,7 @@ import { Dbget } from '../services/db.service';
 
 const getApi = async (req: Request, res: Response) => {
   try {
-    res.send(await Dbget(req.params.id));
+    res.send(await Dbget(+req.params.id));
   } catch (err) {
     if (err instanceof Error) {
       res.json({ Error: true, Message: 'Error execute sql' });
